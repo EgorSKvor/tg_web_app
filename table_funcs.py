35a0c1f97c136d4f3d579863a4964217dfaa55bb
Egor <ee3068316@gmail.com>
2023-09-02 17:03:09 +0300

    tt

--- table_funcs.py
+++ table_funcs.py
@@ -3,5 +3,6 @@ import json
 
 
 def to_table(user_data):
-    df = pd.DataFrame(json.loads(user_data))
+    d_data = json.loads(user_data)
+    df = pd.DataFrame.from_dict(d_data)
     df.to_excel('application_table.xlsx')
