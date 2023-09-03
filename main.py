d6331cb2236415a38e7f96cbdae4b5c63a9ce038
Egor <ee3068316@gmail.com>
2023-09-03 14:44:06 +0300

    v3

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher.storage import FSMContext
 from aiogram.types.web_app_info import WebAppInfo
-from table_funcs import to_table
+from table_funcs import to_csv
+import time
 
 bot = Bot(token=TOKEN)
 storage = MemoryStorage()
@@ -23,6 +24,20 @@ class AnketStates(StatesGroup):
 @dp.message_handler(text='Привет')
 async def hello_func(message: Message):
     await message.answer(text=f'Привет, {message.from_user.first_name}')
+
+
+@dp.message_handler(commands=['start'])
+async def hi_message(message: Message):
+    time.sleep(3)
+    await message.answer(text=f'Уже совсем скоро ты попадёшь на мероприятие, которое станет одним из лучших дней твоей жизни.\n\nПервое, о чем бы хотелось сказать, что мероприятие строго для студентов Инженерной академии. Мы любим все факультеты, но отпраздновать хотим конкретно с вами!\n\nЖдём тебя по адресу … к этому времени …')
+    time.sleep(5)
+    await message.answer(text=f'Мы хотим сделать так, чтобы все прошло идеально и не было никаких казусов. Давайте создадим нечто действительно стоящее и отдохнем без стресса! Поэтому хотелось бы заранее обговорить все правила.\n\nПРЕДУПРЕЖДАЕМ:\n\n⁃ МЕРОПРИЯТИЕ СТРОГО 18+\n⁃ За нанесение ущерба оборудованию и прочему имуществу возложена система штрафов. \n⁃ Мы не несём ответственности за потерянные вещи.\n⁃ Вход строго по спискам.\n⁃ За всем будет следить команда организаторов и охрана. Помимо этого, стоит помнить о наличии камер, которые будут везде. \n⁃ За несоблюдение правил поведения, охрана имеет полное право вывести вас с мероприятия. \n\nЗАПРЕЩЕНЫ: \n\n⁃ Драки.\n⁃ Холодные и огнестрельные оружия.\n⁃ Свой алкоголь.\n⁃ Нарушение дисциплинарного порядка.\n⁃ Наличие запрещенных веществ. \n⁃ Курение сигарет на территории пространства (кроме электронных сигарет и электронных систем для нагревания табака).\n⁃ Проход в зону бара, стола диджея и VIP-зоны для организаторов.')
+    time.sleep(10)
+    await message.answer(text=f'ЧТО ТЕБЯ ЖДЁТ? \n\n⁃ FREEBAR и закуски (всё входит в стоимость билета). На баре будет представлено меню.\n⁃ Крутая, интерактивная программа с танцами и весельем. \n⁃ Хорошее настроение, новые знакомства и просто крутая тусовка!')
+    time.sleep(5)
+    await message.answer(text=f'ЧТО ПО ОПЛАТЕ?\n\nСтоимость билета 1500₽. Перевод по реквизитам.\n\n!! В случае исключения с мероприятия из-за нарушения правил пребывания - возврат средств не предусмотрен.')
+    time.sleep(5)
+    await message.answer(text=f'Дорогие студенты!\n\nЖелаем вам провести время с удовольствием, отдохнуть и получить большое количество ярких и классных эмоций! Ждём тебя 16.09.2023.')
 
 
 @dp.message_handler(commands='anket')
@@ -46,6 +61,7 @@ async def get_napr(message: Message, state: FSMContext):
     data['napr'] = message.text
     await message.answer(text=f'Введите дату рождения')
     await AnketStates.wait_age.set()
+
 
 @dp.message_handler(state=AnketStates.wait_age)
 async def get_age(message: Message, state: FSMContext):
@@ -76,7 +92,8 @@ async def start_web_app(message: Message):
 async def get_data_from_web(message: Message):
     print(message.web_app_data.data, type(message.web_app_data.data))
     content = message.web_app_data.data
-    to_table(content)
+    # print(to_table(content))
+    to_csv(content)
     await message.answer(message.web_app_data.data)
 if __name__ == '__main__':
     executor.start_polling(dp)
--- table_funcs.py
+++ table_funcs.py
@@ -1,8 +1,16 @@
 import pandas as pd
 import json
+import csv
 
 
-def to_table(user_data):
+def to_csv(user_data):
     d_data = json.loads(user_data)
-    df = pd.DataFrame.from_dict(d_data)
-    df.to_excel('application_table.xlsx')
+    # df = pd.DataFrame(columns=['FIO', 'NAPR', 'AGE', 'WHYYOU'])
+    # df = pd.DataFrame()
+    with open('application_table.csv', 'a', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=d_data.keys())
+        writer.writeheader()
+        writer.writerow(d_data)
+
+
+# def to_table(csv):
